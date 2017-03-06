4f537857672a0c66005175b086cb3cbf7fbb758a
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2017-03-06 16:31:52 +0100

    use the extension

--- src/RavuAlHemioIcingaStatusBundle.php
+++ src/RavuAlHemioIcingaStatusBundle.php
@@ -3,7 +3,12 @@
 namespace RavuAlHemio\IcingaStatusBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use RavuAlHemio\IcingaStatusBundle\DependencyInjection\IcingaStatusExtension;
 
 class RavuAlHemioIcingaStatusBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new IcingaStatusExtension();
+    }
 }

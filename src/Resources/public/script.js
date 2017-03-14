f26e05f7db082d776d01f8ad286ec62be6daae87
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2017-03-14 10:52:54 +0100

    attempt fixed header

---
+++ src/Resources/public/script.js
@@ -0,0 +1,28 @@
+var IcingaStatus;
+(function (IcingaStatus) {
+    function matrixFloatingHeader() {
+        window.addEventListener("load", performMatrixFloatingHeader);
+    }
+    IcingaStatus.matrixFloatingHeader = matrixFloatingHeader;
+    function performMatrixFloatingHeader() {
+        var firstHostNameElement = document.querySelector(".service-matrix .host-name");
+        var firstHostNameElementSize = elementSize(firstHostNameElement);
+        var hostHeaderElement = document.querySelector(".service-matrix .host-header");
+        hostHeaderElement.style.width = firstHostNameElementSize[0] + "px";
+        var tableHead = document.querySelector(".service-matrix thead");
+        var tableHeadSize = elementSize(tableHead);
+        var spacerRow = document.createElement("tr");
+        var spacerCell = document.createElement("td");
+        spacerCell.style.height = tableHeadSize[1] + "px";
+        spacerRow.appendChild(spacerCell);
+        tableHead.appendChild(spacerRow);
+        tableHead.style.position = "fixed";
+    }
+    function elementSize(element) {
+        var elementStyle = window.getComputedStyle(element);
+        var width = element.clientWidth - (parseFloat(elementStyle.paddingLeft) + parseFloat(elementStyle.paddingRight));
+        var height = element.clientHeight - (parseFloat(elementStyle.paddingTop) + parseFloat(elementStyle.paddingBottom));
+        return [width, height];
+    }
+})(IcingaStatus || (IcingaStatus = {}));
+//# sourceMappingURL=script.js.map
